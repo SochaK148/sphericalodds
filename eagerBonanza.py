13a68071d1c7438bf55335d4ba7363064260079c
ksakacep <konrad.socha10@wp.pl>
2018-10-22 17:37:54 +0200

    reading from file, adding edges

--- eagerBonanza.py
+++ eagerBonanza.py
@@ -1,10 +1,19 @@
 import matgen as gen
 
+
 class Graph:
-    def __init__(self, v):
+    def __init__(self, v, edges):
         self.vertices = v
         self.edges = [[0 for a in range(v)] for b in range(v)]
         self.colours = [0] * v
+        for e in edges:
+            self.add_edge(e)
+
+    def add_edge(self, new):
+        rx = int(new[0])
+        ry = int(new[1])
+        self.edges[rx][ry] = 1
+        self.edges[ry][rx] = 1
 
     def is_safe(self, v, colour):
         for i in range(self.vertices):
@@ -23,10 +32,11 @@ class Graph:
                     return True
             c += 1
 
+
 v = 4
 e = 5
 g = Graph(v)
-g.edges = gen.randAdjMatrix(v, e)
+g.edges = gen.rand(v, e)
 g.graph_colouring()
 for col in g.colours:
     print(col)
--- matgen.py
+++ matgen.py
@@ -1,17 +1,28 @@
 import random as rng
 
-def randAdjMatrix(v, e):
-  matrix = [0] * v
-  for i in range(v):
-    matrix[i] = [0] * v
-  for i in range(e):
+
+def rand(v, e):
+    matrix = [0] * v
+    for i in range(v):
+        matrix[i] = [0] * v
+    for i in range(e):
+        edges = []
+        rx = -1
+        ry = -1
+        while rx == ry or (rx, ry) in edges:
+            rx = rng.randint(0, v-1)
+            ry = rng.randint(0, v-1)
+        edges.append((rx, ry))
+        matrix[rx][ry] = 1
+        matrix[ry][rx] = 1
+    return matrix
+
+
+def read(filename):
     edges = []
-    rx = -1
-    ry = -1
-    while rx == ry or (rx,ry) in edges:
-      rx = rng.randint(0, v-1)
-      ry = rng.randint(0, v-1)
-    edges.append((rx,ry))
-    matrix[rx][ry] = 1
-    matrix[ry][rx] = 1
-  return matrix
+    with open(filename) as f:
+        temp = [line[:-1] for line in f]
+        vertices = int(temp[0])
+        for line in temp[1:]:
+            edges.append([int(x) for x in line.split(' ')])
+        return [vertices, edges]
